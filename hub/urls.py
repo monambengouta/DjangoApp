bffcaf81f2ee73525bca698f6925cf0b75399f71
monambengouta <monam.bengouta@esprit.tn>
2022-03-03 16:43:47 +0100

    try all type of forms

--- hub/urls.py
+++ hub/urls.py
@@ -1,9 +1,11 @@
 from unicodedata import name
 from django.urls import path
-from .views import homePage ,student_list,student_details,StudentListView,StudentDetailView
+from .views import homePage ,student_list,student_details,StudentListView,StudentDetailView,createStudent,addStudentForm,StudentCreateView
 
 urlpatterns = [
     path('home/',homePage,name='home'),
     path('liststudent/',StudentListView.as_view(),name='student_list'),
     path('getstudent/<int:pk>',StudentDetailView.as_view(),name='student_details'),
+    path('addstudent/',StudentCreateView.as_view(),name='createstudent'),
+    #path('createstudent/',createStudent,name='createstudent')
 ]
